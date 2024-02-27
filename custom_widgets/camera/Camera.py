0e9cadeac2a989676638ac3604adb48f7bac5023
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-02-27 11:25:55 +0100

    -Update CameraWorker to handle image update -Update RecognitionWorker to handle detection and recognition

--- custom_widgets/camera/Camera.py
+++ custom_widgets/camera/Camera.py
@@ -1,23 +1,27 @@
-from PySide6.QtCore import Slot
+from PySide6.QtCore import Slot ,QThread
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import  QWidget
 from .CameraWorker import  CameraWorker
 from .RecognitionWorker import RecognitionWorker
 from .Camera_ui import Ui_Camera
-
-
+import cv2
 
 class CameraWidget(Ui_Camera ,QWidget):
+    capture = cv2.VideoCapture(0)
+    capture.set(cv2.CAP_PROP_FPS ,30)
 
     def __init__(self):
         super(CameraWidget, self).__init__()
         self.setupUi(self)
-        self.cameraWorker = CameraWorker(self)
+        self.cameraWorker = CameraWorker(self.capture)
         self.cameraWorker.update_frame.connect(self.setImage)
-        self.recognitionWorker = RecognitionWorker(self.cameraWorker)
-        self.recognitionWorker.recognizedFace.connect(self.handleRecognition)
+        self.recognitionWorker = RecognitionWorker(self.capture)
+        # self.cameraWorker.faceDetected.connect(self.recognitionWorker.recognitionProcess)
+        # self.recognitionWorker.recognizedFace.connect(self.handleRecognition)
         self.cameraWorker.start()
         self.recognitionWorker.start()
+        self.cameraWorker.setPriority(QThread.Priority.HighestPriority)
+        self.recognitionWorker.setPriority(QThread.Priority.LowPriority)
 
     def closeEvent(self, event):
         self.cameraWorker.killWorker()
