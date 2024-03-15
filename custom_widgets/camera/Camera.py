f03ecab032da53402e5d6dc5b07d4a3ac6edc204
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-03-16 00:49:19 +0100

    -Add functionality to create multiple camera Label

--- custom_widgets/camera/Camera.py
+++ custom_widgets/camera/Camera.py
@@ -2,7 +2,7 @@ from PySide6.QtCore import Slot ,Qt
 from PySide6.QtGui import QPixmap ,QImage ,QAction
 from PySide6.QtWidgets import  QWidget ,QLabel ,QMenu
 from .CameraWorker import  CameraWorker
-from .RecognitionWorker import RecognitionWorker
+
 from .Camera_ui import Ui_Camera
 import cv2
 
@@ -10,32 +10,35 @@ class CameraWidget(Ui_Camera ,QWidget):
 	def __init__(self):
 		super(CameraWidget, self).__init__()
 		self.setupUi(self)
-		self.label_1 = QLabel('LoadCamera')
-		self.label_2 = QLabel('LoadCamera')
-		
-		self.label_2.hide()
-		self.cameraGrid.addWidget(self.label_1 ,0 ,0)
-		self.cameraGrid.addWidget(self.label_2 ,0 ,1)
-
+		self.cameraLabels = [self.label_1 ,self.label_2 ,self.label_3 ,self.label_4]
+		self.oneCam()
+		cap = 'http://192.168.1.3:4747/video'
 		self.cameraWorker = CameraWorker(0)
 		self.cameraWorker.updateFrame.connect(self.setImageLabel_1)
 		# self.cameraWorker2 = CameraWorker('http://192.168.1.3:4747/video')
 		# self.cameraWorker2.updateFrame.connect(self.setImageLabel_2)
 		self.oneCamButton.clicked.connect(self.oneCam)
 		self.twoCamButton.clicked.connect(self.twoCam)
+		self.fourCamButton.clicked.connect(self.fourCam)
 		self.cameraWorker.start()
 	
 	def oneCam(self):
-		if self.label_2 is not None:
-			self.label_2.hide()
+		for label in self.cameraLabels[1:]:
+			label.hide()
 		# if self.cameraWorker2.isRunning():
 		# 	self.cameraWorker2.killWorker()
 		
 	def twoCam(self):
-		if self.label_2 is not None:
-			self.label_2.show()
+		self.cameraLabels[1].show()
+		for label in self.cameraLabels[2:]:
+			label.hide()
 		# if not self.cameraWorker2.isRunning():
 		# 	self.cameraWorker2.start()
+	
+	def fourCam(self):
+		for label in self.cameraLabels[1:]:
+			label.show()
+
 
 
 	def closeEvent(self, event):
