c367742ee167a6f8970ab3f22cb4db3e33f968de
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-03-26 12:33:53 +0100

    -Redesign Setting UI -Showing each camera device connected into a Table

--- custom_widgets/setting/Setting.py
+++ custom_widgets/setting/Setting.py
@@ -1,30 +1,112 @@
-import cv2 as cv 
-import cvzone
-from ultralytics import YOLO
-import face_recognition
-from .Setting_ui import Ui_Setting
-from PySide6.QtCore import QThread
-from PySide6.QtWidgets import  QWidget
+from PySide6.QtWidgets import  QWidget ,QTableWidgetItem ,QCheckBox
+from PySide6.QtMultimedia import QMediaDevices
+from PySide6.QtCore import QThread ,Slot 
 from databasemanager import DataBaseManager
+from .Setting_ui import Ui_Setting
+from deepface import DeepFace
+import numpy as np
+import cv2 
 
 class Setting (Ui_Setting,QWidget):
-    def __init__(self):
-        super(Setting,self).__init__()
-        self.setupUi(self)
-        self.checkBox.stateChanged.connect(self.faceDetection)
-        self.checkBox_2.stateChanged.connect(self.faceRecognition)
-        self.checkBox_3.stateChanged.connect(self.tracking)
-    
-    def faceDetection(self,state):
-        if state == 2: 
-            print("Checkbox is checked")
-        else:
-            print("Checkbox is unchecked")
-            # self.th.quit()
+	def __init__(self):
+		super(Setting,self).__init__()
+		self.setupUi(self)
+		self.recognitionWorkerDict:dict = {}
+		self.MAXRECOGNITIONWORKER = 4
+		self.loadCameras()
 
-    def faceRecognition(self,state):
-        if state == 2 :
-            print ("okacha kfkjfosk is tchekdk ")
+	def loadCameras(self):
+		self.cameraTableWidget.clear()
+		cameras = QMediaDevices.videoInputs()
+		self.cameraTableWidget.setRowCount(len(cameras))
+		self.cameraTableWidget.setColumnCount(3)
+		for row ,camera in enumerate(cameras):
+			cameraId  = camera.id().data().decode()
+			name = camera.description()
+			self.cameraTableWidget.setItem(row ,0 ,QTableWidgetItem(cameraId))
+			self.cameraTableWidget.setItem(row ,1 ,QTableWidgetItem(name))
+			checkbox = QCheckBox()
+			checkbox.stateChanged.connect(lambda state: self.startRecognition(cameraId , state))
+			self.cameraTableWidget.setCellWidget(row ,2 ,checkbox)
+			self.cameraTableWidget.hideColumn(0)
 
-    def tracking (self):
-        print ("tracking is clicked ")
+	def startRecognition(self ,cameraId ,state):
+		totalCheckedBox = self.getTotalCheckedBox()
+		if not state:
+			print('unchecked')
+			if cameraId in self.recognitionWorkerDict:
+				self.recognitionWorkerDict[cameraId].killWorker() 
+				del self.recognitionWorkerDict[cameraId] 
+		elif totalCheckedBox < self.MAXRECOGNITIONWORKER :
+			print('checked')
+			if cameraId not in self.recognitionWorkerDict: 
+				worker = RecognitionWorker(cameraId)
+				self.recognitionWorkerDict[cameraId] = worker 
+				worker.start()
+			
+	def getTotalCheckedBox(self) -> int:
+		totalChecked = 0
+		for row in range(self.cameraTableWidget.rowCount()):
+			checkbox:QCheckBox = self.cameraTableWidget.cellWidget(row, 2)
+			if checkbox.isChecked():
+				totalChecked += 1
+		return totalChecked
+
+
+class RecognitionWorker(QThread):
+
+	def __init__(self ,cameraId ,parent=None):
+		QThread.__init__(self , parent)
+		self.databaseManager = DataBaseManager()
+		self.knownEmbeddings = self.databaseManager.getEncodingArray()
+		self.cameraId = cameraId
+		self.counter = 0
+		self.status = True
+		self.cap = None
+
+	def run(self):
+		self.cap = cv2.VideoCapture(self.cameraId)
+		fps = self.cap.get(cv2.CAP_PROP_FPS)
+		print(f"The FPS in the camera with ID {self.cameraId} are {fps}")
+		while self.status:
+			# if self.counter % 30 != 0:
+			# 	self.counter += 1
+			# 	continue
+			ret, fram = self.cap.read()
+			if ret:
+				try:
+					results = DeepFace.represent(fram, model_name='Facenet512', detector_backend='yolov8')
+					for index ,face in enumerate(results):
+						print(index)
+						embedding = np.array(face["embedding"])
+						self.checkEmbeddingMatch(self.knownEmbeddings ,embedding)
+				except Exception as e:
+					print(f'Face not found: {e}')
+			# self.counter = 0
+
+	def checkEmbeddingMatch(self ,knownEmbeddings:dict ,unknownEmbedding:np.ndarray ,threshold:int=20):
+		matches = {}
+		for personId , embeddings in knownEmbeddings.items():
+			matchCounter = 0
+			for embedding in embeddings:
+				knownEmbedding = np.array(embedding)
+				distance_vector = np.square(unknownEmbedding - knownEmbedding)
+				distance = np.sqrt(distance_vector.sum())
+				if distance < threshold:
+					matchCounter += 1
+			matches[personId] = (matchCounter/len(embeddings)) * 100
+		if max(matches.values())< 80:
+			#TODO:Implement the logique for saving the fram and sending alert for admin
+			print('who are you identify you self /"""" ')
+
+	@Slot(cv2.Mat)
+	def setFram(self ,fram):
+		self.fram = fram
+
+	def killWorker(self):
+		self.status = False
+		if self.cap is not None:
+			self.cap.release()
+		self.quit()
+		self.wait()
+		self = None
--- custom_widgets/setting/Setting_ui.py
+++ custom_widgets/setting/Setting_ui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'Setting.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.6.2
+## Created by: Qt User Interface Compiler version 6.6.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QFrame, QGridLayout,
-    QHBoxLayout, QLabel, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QHeaderView,
+    QPushButton, QSizePolicy, QSpacerItem, QTabWidget,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
 
 class Ui_Setting(object):
     def setupUi(self, Setting):
@@ -38,101 +38,75 @@ class Ui_Setting(object):
 "}")
         self.gridLayout = QGridLayout(Setting)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.horizontalSpacer = QSpacerItem(160, 20, QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout.addItem(self.horizontalSpacer, 1, 0, 1, 1)
-
-        self.verticalSpacer = QSpacerItem(20, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer, 0, 1, 1, 1)
-
-        self.frame = QFrame(Setting)
-        self.frame.setObjectName(u"frame")
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.verticalLayout = QVBoxLayout(self.frame)
-        self.verticalLayout.setSpacing(7)
+        self.tabWidget = QTabWidget(Setting)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setTabPosition(QTabWidget.West)
+        self.tabWidget.setTabShape(QTabWidget.Rounded)
+        self.tabWidget.setElideMode(Qt.ElideMiddle)
+        self.faceRecognitionTab = QWidget()
+        self.faceRecognitionTab.setObjectName(u"faceRecognitionTab")
+        self.verticalLayout = QVBoxLayout(self.faceRecognitionTab)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 50, -1, 400)
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.label = QLabel(self.frame)
-        self.label.setObjectName(u"label")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.horizontalLayout.addWidget(self.label)
+        self.horizontalLayout.addItem(self.horizontalSpacer)
 
-        self.checkBox = QCheckBox(self.frame)
-        self.checkBox.setObjectName(u"checkBox")
-        self.checkBox.setMinimumSize(QSize(0, 0))
-        self.checkBox.setMaximumSize(QSize(20, 16777215))
+        self.refreshButton = QPushButton(self.faceRecognitionTab)
+        self.refreshButton.setObjectName(u"refreshButton")
 
-        self.horizontalLayout.addWidget(self.checkBox)
+        self.horizontalLayout.addWidget(self.refreshButton)
 
-        self.horizontalLayout.setStretch(0, 3)
-        self.horizontalLayout.setStretch(1, 1)
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_2 = QLabel(self.frame)
-        self.label_2.setObjectName(u"label_2")
+        self.cameraTableWidget = QTableWidget(self.faceRecognitionTab)
+        if (self.cameraTableWidget.columnCount() < 2):
+            self.cameraTableWidget.setColumnCount(2)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.cameraTableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.cameraTableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        self.cameraTableWidget.setObjectName(u"cameraTableWidget")
+        self.cameraTableWidget.horizontalHeader().setCascadingSectionResizes(False)
+        self.cameraTableWidget.horizontalHeader().setHighlightSections(True)
+        self.cameraTableWidget.horizontalHeader().setProperty("showSortIndicator", False)
+        self.cameraTableWidget.horizontalHeader().setStretchLastSection(False)
+        self.cameraTableWidget.verticalHeader().setVisible(True)
+        self.cameraTableWidget.verticalHeader().setCascadingSectionResizes(False)
+        self.cameraTableWidget.verticalHeader().setStretchLastSection(False)
 
-        self.horizontalLayout_2.addWidget(self.label_2)
+        self.verticalLayout.addWidget(self.cameraTableWidget)
 
-        self.checkBox_2 = QCheckBox(self.frame)
-        self.checkBox_2.setObjectName(u"checkBox_2")
-        self.checkBox_2.setMaximumSize(QSize(20, 16777215))
+        self.tabWidget.addTab(self.faceRecognitionTab, "")
+        self.recordsTab = QWidget()
+        self.recordsTab.setObjectName(u"recordsTab")
+        self.tabWidget.addTab(self.recordsTab, "")
+        self.notificationTab = QWidget()
+        self.notificationTab.setObjectName(u"notificationTab")
+        self.tabWidget.addTab(self.notificationTab, "")
 
-        self.horizontalLayout_2.addWidget(self.checkBox_2)
-
-        self.horizontalLayout_2.setStretch(0, 3)
-        self.horizontalLayout_2.setStretch(1, 1)
-
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
-
-        self.horizontalLayout_3 = QHBoxLayout()
-        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.label_3 = QLabel(self.frame)
-        self.label_3.setObjectName(u"label_3")
-
-        self.horizontalLayout_3.addWidget(self.label_3)
-
-        self.checkBox_3 = QCheckBox(self.frame)
-        self.checkBox_3.setObjectName(u"checkBox_3")
-        self.checkBox_3.setMaximumSize(QSize(20, 16777215))
-
-        self.horizontalLayout_3.addWidget(self.checkBox_3)
-
-        self.horizontalLayout_3.setStretch(0, 3)
-        self.horizontalLayout_3.setStretch(1, 1)
-
-        self.verticalLayout.addLayout(self.horizontalLayout_3)
-
-
-        self.gridLayout.addWidget(self.frame, 1, 1, 1, 1)
-
-        self.horizontalSpacer_2 = QSpacerItem(160, 20, QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout.addItem(self.horizontalSpacer_2, 1, 2, 1, 1)
-
-        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer_2, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
 
 
         self.retranslateUi(Setting)
+
+        self.tabWidget.setCurrentIndex(0)
+
 
         QMetaObject.connectSlotsByName(Setting)
     # setupUi
 
     def retranslateUi(self, Setting):
         Setting.setWindowTitle(QCoreApplication.translate("Setting", u"Form", None))
-        self.label.setText(QCoreApplication.translate("Setting", u"Face Detection", None))
-        self.checkBox.setText("")
-        self.label_2.setText(QCoreApplication.translate("Setting", u"Face recognition", None))
-        self.checkBox_2.setText("")
-        self.label_3.setText(QCoreApplication.translate("Setting", u"tracking", None))
-        self.checkBox_3.setText("")
+        self.refreshButton.setText(QCoreApplication.translate("Setting", u"refresh", None))
+        ___qtablewidgetitem = self.cameraTableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Setting", u"Camera Name", None));
+        ___qtablewidgetitem1 = self.cameraTableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Setting", u"Selected", None));
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.faceRecognitionTab), QCoreApplication.translate("Setting", u"Face Recognition", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.recordsTab), QCoreApplication.translate("Setting", u"Records", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.notificationTab), QCoreApplication.translate("Setting", u"Notification", None))
     # retranslateUi
 
