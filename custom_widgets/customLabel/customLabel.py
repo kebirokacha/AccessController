924a219b2e4c69ba4a19912ae0f1813aa2ee4ed7
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-03-29 17:44:53 +0100

    -Rename Draggable label into custom label

--- custom_widgets/customLabel/customLabel.py
+++ custom_widgets/customLabel/customLabel.py
@@ -1,17 +1,19 @@
 from PySide6.QtWidgets import QLabel ,QListWidget
-from custom_widgets.camera.CameraWorker import  CameraWorker
 from PySide6.QtCore import Signal ,Slot 
 from PySide6.QtGui import QPixmap , QImage
+from ..camera.CameraWorker import  CameraWorker
+from ..setting.Setting import Setting
 
 
-class DraggableLabel(QLabel):
+class CustomLabel(QLabel):
 	startWorker  = Signal()
 	removeItem = Signal(str)
 	cameraId = None
 	cameraWorker = None
 
-	def __init__(self ,text :QListWidget ,parent=None):
-		super(DraggableLabel ,self).__init__(parent)
+	def __init__(self ,text:str ,setting:Setting ,parent=None):
+		super(CustomLabel ,self).__init__(parent)
+		self.setting = setting
 		self.cameraText = text
 		self.setText(self.cameraText)
 		self.setAcceptDrops(True)
@@ -38,12 +40,11 @@ class DraggableLabel(QLabel):
 		print(f'starting camera with id:{cameraId}')
 		self.cameraId = cameraId
 		self.cameraWorker = CameraWorker(cameraId)
+		self.cameraWorker.requestCapture.connect(self.setting.requestCapture)
 		self.cameraWorker.updateFrame.connect(self.setImageLabel)
+		self.cameraWorker.refreshCapture.connect(self.setting.refreshCapture)
+		self.setting.sendCapture.connect(self.cameraWorker.reciveCapture)
 		self.cameraWorker.start()
-	
-	def updateRecognitionWorkerInfo(self):
-		if self.cameraWorker is not None:
-			self.cameraWorker.recognitionWorker
 
 	def killWorker(self):
 		if self.cameraWorker is not None and self.cameraWorker.isRunning():
