8be921c9813eb58d3bb9bb5609fcdb043a608772
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-03-31 00:18:41 +0100

    -Refacture the custom label so it send signal to the setting to initialize threads

--- custom_widgets/customLabel/customLabel.py
+++ custom_widgets/customLabel/customLabel.py
@@ -1,19 +1,22 @@
-from PySide6.QtWidgets import QLabel ,QListWidget
+from PySide6.QtWidgets import QLabel
 from PySide6.QtCore import Signal ,Slot 
 from PySide6.QtGui import QPixmap , QImage
-from ..camera.CameraWorker import  CameraWorker
 from ..setting.Setting import Setting
 
 
 class CustomLabel(QLabel):
-	startWorker  = Signal()
+	startLiveSignal  = Signal(str)
+	endLiveSignal  = Signal(str)
 	removeItem = Signal(str)
-	cameraId = None
+	captureId = None
 	cameraWorker = None
 
 	def __init__(self ,text:str ,setting:Setting ,parent=None):
 		super(CustomLabel ,self).__init__(parent)
 		self.setting = setting
+		self.setting.checkThreads.connect(self.connectThread)
+		self.startLiveSignal.connect(self.setting.startLive)
+		self.endLiveSignal.connect(self.setting.killThreads)
 		self.cameraText = text
 		self.setText(self.cameraText)
 		self.setAcceptDrops(True)
@@ -30,25 +33,27 @@ class CustomLabel(QLabel):
 		if event.mimeData().hasText():
 			info = event.mimeData().text()
 			info = info.split(',')
-			cameraId , cameraName = info[0] ,info[1]
-			self.startCameraWorker(cameraId)
+			captureId , cameraName = info[0] ,info[1]
+			self.startLive(captureId)
 			event.acceptProposedAction()
 			self.removeItem.emit(cameraName)
-			
+			self.setAcceptDrops(False)
 
-	def startCameraWorker(self, cameraId):
-		print(f'starting camera with id:{cameraId}')
-		self.cameraId = cameraId
-		self.cameraWorker = CameraWorker(cameraId)
-		self.cameraWorker.requestCapture.connect(self.setting.requestCapture)
-		self.cameraWorker.updateFrame.connect(self.setImageLabel)
-		self.cameraWorker.refreshCapture.connect(self.setting.refreshCapture)
-		self.setting.sendCapture.connect(self.cameraWorker.reciveCapture)
-		self.cameraWorker.start()
-
-	def killWorker(self):
-		if self.cameraWorker is not None and self.cameraWorker.isRunning():
-			self.cameraWorker.killWorker()
+	def startLive(self, captureId):
+		print(f'starting camera with id:{captureId}')
+		self.captureId = captureId
+		self.startLiveSignal.emit(self.captureId)
+		
+	def close(self):
+		if self.captureId is not None:
+			self.endLiveSignal.emit(self.captureId)
+		
+	@Slot()
+	def connectThread(self):
+		captures = self.setting.framReaderThreads
+		if self.captureId is not None and self.captureId in captures:
+			print(f' capture id in the custom label{self.captureId}')
+			captures[self.captureId].updateFrame.connect(self.setImageLabel)
 
 	@Slot(QImage)
 	def setImageLabel(self, image:QImage):
