0d9b13a6847dc52b155ec0fae8e5b3156df944d2
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-04-05 12:35:44 +0100

    -Fix closing latency bug

--- custom_widgets/setting/Setting.py
+++ custom_widgets/setting/Setting.py
@@ -32,35 +32,39 @@ class Setting (Ui_Setting,QWidget):
 			checkbox.setDisabled(True)
 			checkbox.stateChanged.connect(lambda state: self.startRecognition(captureId , state))
 			self.cameraTableWidget.setCellWidget(row ,2 ,checkbox)
-			self.cameraTableWidget.hideColumn(0)
+			# self.cameraTableWidget.hideColumn(0)
 			
 		#FIXME: Testing purpose (Remove Later)
-		captureIP = 'http://192.168.1.3:4747/video'
-		self.cameraTableWidget.setItem(row+1 ,0 ,QTableWidgetItem(captureIP))
+		captureIP = 'http://192.168.1.3:8080/video'
+		self.cameraTableWidget.setItem(row+1 ,0 ,QTableWidgetItem('http://192.168.1.3:8080/video'))
 		self.cameraTableWidget.setItem(row+1 ,1 ,QTableWidgetItem('My Phone'))
 		checkbox = QCheckBox()
 		checkbox.setDisabled(True)
-		checkbox.stateChanged.connect(lambda state: self.startRecognition(captureIP , state))
+		checkbox.stateChanged.connect(lambda state: self.startRecognition('http://192.168.1.3:8080/video' , state))
 		self.cameraTableWidget.setCellWidget(row+1 ,2 ,checkbox)
-		self.cameraTableWidget.hideColumn(0)
+		# self.cameraTableWidget.hideColumn(0)
 
 		captureIP = 'http://192.168.1.4:4747/video'
-		self.cameraTableWidget.setItem(row+2 ,0 ,QTableWidgetItem(captureIP))
+		self.cameraTableWidget.setItem(row+2 ,0 ,QTableWidgetItem('http://192.168.1.4:4747/video'))
 		self.cameraTableWidget.setItem(row+2 ,1 ,QTableWidgetItem('Mother phone'))
 		checkbox = QCheckBox()
 		checkbox.setDisabled(True)
-		checkbox.stateChanged.connect(lambda state: self.startRecognition(captureIP , state))
+		checkbox.stateChanged.connect(lambda state: self.startRecognition('http://192.168.1.4:4747/video' , state))
 		self.cameraTableWidget.setCellWidget(row+2 ,2 ,checkbox)
-		self.cameraTableWidget.hideColumn(0)
+		# self.cameraTableWidget.hideColumn(0)
 
 		captureIP = 'http://192.168.1.2:4747/video'
-		self.cameraTableWidget.setItem(row+3 ,0 ,QTableWidgetItem(captureIP))
+		self.cameraTableWidget.setItem(row+3 ,0 ,QTableWidgetItem('http://192.168.1.2:4747/video'))
 		self.cameraTableWidget.setItem(row+3 ,1 ,QTableWidgetItem('Saadane phone'))
 		checkbox = QCheckBox()
 		checkbox.setDisabled(True)
-		checkbox.stateChanged.connect(lambda state: self.startRecognition(captureIP , state))
+		checkbox.stateChanged.connect(lambda state: self.startRecognition('http://192.168.1.2:4747/video' , state))
 		self.cameraTableWidget.setCellWidget(row+3 ,2 ,checkbox)
-		self.cameraTableWidget.hideColumn(0)
+		# self.cameraTableWidget.hideColumn(0)
+  
+	def loadPath(self):
+		# Check the database for path if existe
+		pass
 
 	def startRecognition(self ,captureId ,state):
 		totalCheckedBox = self.getTotalCheckedBox()
@@ -77,8 +81,11 @@ class Setting (Ui_Setting,QWidget):
 					framReaderThread = self.framReaderThreads[captureId]
 					framReaderThread.sendFrame.connect(recognitonThread.setFrame)
 					framReaderThread.start()
+					print(f'framReaderThread {framReaderThread} is started ')
 				self.recognitionThreads[captureId] = recognitonThread
 				recognitonThread.start()
+				print(f'recognitonThread {recognitonThread} is started ')
+
 		print(f'the framReaderThreads in Setting is {self.framReaderThreads}')
 		print(f'the recognitionThreads in Setting is {self.recognitionThreads}')
 
@@ -97,22 +104,36 @@ class Setting (Ui_Setting,QWidget):
 				checkbox = self.cameraTableWidget.cellWidget(row, 2)
 				return checkbox
 		return None
+
+	def killAllThreads(self):
+		for framReadingThread in self.framReaderThreads.values():
+			framReadingThread.killThread()
+		for recognitionThread in self.recognitionThreads.values():
+			recognitionThread.killThread()
 	
-	@Slot(str)
-	def startLive(self ,captureId):
+	@Slot(str ,str)
+	def startLive(self ,captureId ,captureName):
 		if captureId not in self.framReaderThreads:
-			frameReadingThread = FrameReadingThread(captureId)
+			frameReadingThread = FrameReadingThread(captureId ,captureName)
 			self.framReaderThreads[captureId] = frameReadingThread
 			self.checkThreads.emit()
 			checkbox = self.getCheckboxForCaptureId(captureId)
 			checkbox.setEnabled(True)
-			checkbox.setChecked(not checkbox.isChecked())
+			checkbox.setChecked(True)
 	
 	@Slot(str)
 	def killThreads(self ,captureId):
+		#FIXME: reset the checkbox to default
 		if captureId in self.framReaderThreads:
+			checkbox = self.getCheckboxForCaptureId(captureId)
+			checkbox.setChecked(False)
+			checkbox.setDisabled(True)
 			self.framReaderThreads[captureId].killThread()
 			del self.framReaderThreads[captureId]
+			print('framReaderThread was killed ')
 		if captureId in self.recognitionThreads:
 			self.recognitionThreads[captureId].killThread()
 			del self.recognitionThreads[captureId]
+			print('recognitionThread was killed ')
+		print('KillThreads completed')
+		
