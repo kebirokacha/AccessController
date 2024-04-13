46cfa35a79b2e5d25a0f54120b33d62e0f3dc643
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-04-13 20:20:11 +0100

    -Add stackwidget that have the pictureViewer and videoPlayer -Modify UI

--- custom_widgets/records/records.py
+++ custom_widgets/records/records.py
@@ -1,9 +1,12 @@
 from PySide6.QtWidgets import QWidget ,QListWidgetItem
-from PySide6.QtCore import Slot
+from PySide6.QtCore import Slot ,QUrl 
+from PySide6.QtGui import QPixmap
 from PySide6.QtMultimediaWidgets import QVideoWidget
 from PySide6.QtMultimedia import QMediaPlayer
 from .records_ui import  Ui_Records
 from ..videoCardInfo.videoCardInfo import VideoCardInfo
+from ..pictureCardInfo.pictureCardInfo import PictureCardInfo
+from databasemanager import DataBaseManager
 import os
 
 class Records(Ui_Records ,QWidget):
@@ -13,54 +16,71 @@ class Records(Ui_Records ,QWidget):
 		self.rightSideBar.setMinimumWidth(600)
 		self.rightSideBar.hide()
 		self.toggleSideBarButton.clicked.connect(self.toggleSideBar)
-		self.player = QMediaPlayer()
-		self.videoWidget = QVideoWidget()
-		self.player.setVideoOutput(self.videoWidget)
-		self.videoWidgetFramLayout.addWidget(self.videoWidget)
-		self.listWidget.itemClicked.connect(lambda item :self.selectVideo(item))
-		self.slider.setRange(0, 0)
-		self.slider.sliderMoved.connect(self.set_position)
-		# Connect the player's positionChanged signal to update_slider
-		self.player.positionChanged.connect(self.update_slider)
-		self.player.durationChanged.connect(self.duration_changed)
-		self.player.positionChanged.connect(self.update_current_time_label)
-		self.fillListWidget()
+		self.initializeVideoPlayer()
+		self.videoRadioButton.toggled.connect(self.setVideoPlayer)
+		self.pictureRadioButton.toggled.connect(self.setPictureViewer)
+		self.fillListWidgetWithVideos()
 		
 
-	def set_position(self, position):
+	def setPosition(self, position):
 		self.player.setPosition(position)
 
-	def update_slider(self, position):
+	def updateSlider(self, position):
 		self.slider.setValue(position)
 
-	def duration_changed(self, duration):
-		self.slider.setRange(0, duration)
-
-	def update_current_time_label(self, position):
-		self.currentTimeVideo.setText(self.format_time(position))
-		self.totalTimeVideo.setText(self.format_time(self.player.duration()))
+	def updateCurrentTimeLabel(self, position):
+		self.currentTimeVideo.setText(self.getFormatTime(position))
+		self.totalTimeVideo.setText(self.getFormatTime(self.player.duration()))
 	
-	def duration_changed(self, duration):
+	def durationChanged(self, duration):
 		self.slider.setRange(0, duration)
-		self.totalTimeVideo.setText(self.format_time(duration))
+		self.totalTimeVideo.setText(self.getFormatTime(duration))
 
-	@staticmethod
-	def format_time(ms):
+	def getFormatTime(self ,ms):
 		seconds = ms // 1000
 		minutes = seconds // 60
 		hours = minutes // 60
 		return f"{hours:02d}:{minutes % 60:02d}:{seconds % 60:02d}"
 
-	@Slot()
-	def fillListWidget(self):
-		#FIXME: later you will get the path from setting or database
-		self.listWidget.clear()
-		videoFolderPath = "files_records/videos"
-		videoFiles = [f for f in os.listdir(videoFolderPath) if f.endswith(('.mp4', '.avi', '.mkv'))]
+	def initializeVideoPlayer(self):
+		self.player = QMediaPlayer()
+		self.videoWidget = QVideoWidget()
+		self.player.setVideoOutput(self.videoWidget)
+		self.videoWidgetFramLayout.addWidget(self.videoWidget)
+		self.listWidget.itemClicked.connect(lambda item :self.itemSelected(item))
+		self.slider.setRange(0, 0)
+		self.slider.sliderMoved.connect(self.setPosition)
+		# Connect the player's positionChanged signal to update_slider
+		self.player.positionChanged.connect(self.updateSlider)
+		self.player.durationChanged.connect(self.durationChanged)
+		self.player.positionChanged.connect(self.updateCurrentTimeLabel)
 
+	def setVideoPlayer(self):
+		self.player.setSource(QUrl())
+		self.player.play()
+		self.stackedWidget.setCurrentIndex(0)
+		self.fillListWidgetWithVideos()
+
+	def setPictureViewer(self):
+		self.currentTimeVideo.setText("00:00:00")
+		self.totalTimeVideo.setText("00:00:00")
+		self.pictureViewer.clear()
+		self.stackedWidget.setCurrentIndex(1)
+		self.fillListWidgetWithPictures()
+
+
+	@Slot()
+	def fillListWidgetWithVideos(self):
+		self.listWidget.clear()
+		dataBaseManager = DataBaseManager()
+		recordsFolderPath = dataBaseManager.getRecordsFolderPath()
+		videoFolderPath = os.path.join(recordsFolderPath ,'videos')
+		if not os.path.isdir(videoFolderPath):
+			os.mkdir(videoFolderPath)
+		videoFiles = [f for f in os.listdir(videoFolderPath) if f.endswith(('.mp4', '.avi', '.mkv'))]
 		for videoFile in videoFiles:
 			videoCardInfo = VideoCardInfo()
-			videoCardInfo.refreshVideoList.connect(self.fillListWidget)
+			videoCardInfo.refreshVideoList.connect(self.fillListWidgetWithVideos)
 			videoCardInfo.setupCard({
 					"videoFolderPath":videoFolderPath,
 					"videoFile":videoFile,
@@ -70,17 +90,55 @@ class Records(Ui_Records ,QWidget):
 			self.listWidget.addItem(listWidgetItem)
 			self.listWidget.setItemWidget(listWidgetItem, videoCardInfo)
 
+	@Slot()
+	def fillListWidgetWithPictures(self):
+		self.listWidget.clear()
+		dataBaseManager = DataBaseManager()
+		recordsFolderPath = dataBaseManager.getRecordsFolderPath()
+		picturesFolderPath = os.path.join(recordsFolderPath ,'pictures')
+		if not os.path.isdir(picturesFolderPath):
+			os.mkdir(picturesFolderPath)
+		pictureFiles = [f for f in os.listdir(picturesFolderPath) if f.endswith(('.jpg', '.jpeg'))]
+		for pictureFile in pictureFiles:
+			pictureCardInfo = PictureCardInfo()
+			pictureCardInfo.refreshPicturesList.connect(self.fillListWidgetWithPictures)
+			pictureCardInfo.setupCard({
+					"pictureFolderPath":picturesFolderPath,
+					"pictureFile":pictureFile,
+				})
+			listWidgetItem = QListWidgetItem(self.listWidget)
+			listWidgetItem.setSizeHint(pictureCardInfo.sizeHint())
+			self.listWidget.addItem(listWidgetItem)
+			self.listWidget.setItemWidget(listWidgetItem, pictureCardInfo)
+
+
 	def toggleSideBar(self):
 		toggled = self.rightSideBar.property('toggled')
-		if toggled:
-			self.rightSideBar.setProperty('toggled' ,not toggled)
-			self.rightSideBar.show()
+		if self.videoRadioButton.isChecked():
+			self.fillListWidgetWithVideos()
+		elif self.pictureRadioButton.isChecked():
+			self.fillListWidgetWithPictures()
+		if not toggled:
+			self.showSideBar()
 		else:
-			self.rightSideBar.setProperty('toggled' ,not toggled)
-			self.rightSideBar.hide()
+			self.hideSideBar()
+
+	def hideSideBar(self):
+		self.rightSideBar.setProperty('toggled' ,False)
+		self.rightSideBar.hide()
+
+	def showSideBar(self):
+		self.rightSideBar.setProperty('toggled' ,True)
+		self.rightSideBar.show()
 		
-	def selectVideo(self ,item:QListWidgetItem):
-		videoCardInfo = self.listWidget.itemWidget(item)
-		self.player.setSource(videoCardInfo.videoFilePath)
-		self.player.play()
+		
+	def itemSelected(self ,item:QListWidgetItem):
+		itemWidget = self.listWidget.itemWidget(item)
+		if isinstance(itemWidget ,VideoCardInfo):
+			videoCardInfo:VideoCardInfo = self.listWidget.itemWidget(item)
+			self.player.setSource(QUrl(videoCardInfo.videoFilePath))
+			self.player.play()
+		elif isinstance(itemWidget ,PictureCardInfo):
+			pictureCardInfo:PictureCardInfo = self.listWidget.itemWidget(item)
+			self.pictureViewer.setPixmap(QPixmap(pictureCardInfo.pictureFilePath))
 
--- custom_widgets/records/records_ui.py
+++ custom_widgets/records/records_ui.py
@@ -15,10 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QAbstractSpinBox, QApplication, QComboBox,
-    QDateTimeEdit, QFrame, QGridLayout, QHBoxLayout,
-    QLabel, QListWidget, QListWidgetItem, QPushButton,
-    QRadioButton, QSizePolicy, QSlider, QSpacerItem,
+from PySide6.QtWidgets import (QAbstractItemView, QAbstractSpinBox, QApplication, QDateTimeEdit,
+    QFrame, QGridLayout, QHBoxLayout, QLabel,
+    QListWidget, QListWidgetItem, QPushButton, QRadioButton,
+    QSizePolicy, QSlider, QSpacerItem, QStackedWidget,
     QVBoxLayout, QWidget)
 from resources.ui import resources_rc
 
@@ -51,15 +51,16 @@ class Ui_Records(object):
         self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.radioButton = QRadioButton(self.leftSideBare)
-        self.radioButton.setObjectName(u"radioButton")
+        self.videoRadioButton = QRadioButton(self.leftSideBare)
+        self.videoRadioButton.setObjectName(u"videoRadioButton")
+        self.videoRadioButton.setChecked(True)
 
-        self.horizontalLayout_3.addWidget(self.radioButton)
+        self.horizontalLayout_3.addWidget(self.videoRadioButton)
 
-        self.radioButton_2 = QRadioButton(self.leftSideBare)
-        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.pictureRadioButton = QRadioButton(self.leftSideBare)
+        self.pictureRadioButton.setObjectName(u"pictureRadioButton")
 
-        self.horizontalLayout_3.addWidget(self.radioButton_2)
+        self.horizontalLayout_3.addWidget(self.pictureRadioButton)
 
 
         self.verticalLayout_3.addLayout(self.horizontalLayout_3)
@@ -112,7 +113,14 @@ class Ui_Records(object):
         self.verticalLayout_2 = QVBoxLayout(self.frame)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(6, 6, 6, 6)
-        self.videoWidgetFram = QFrame(self.frame)
+        self.stackedWidget = QStackedWidget(self.frame)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.stackedWidget.setFrameShadow(QFrame.Plain)
+        self.videoPlayerPage = QWidget()
+        self.videoPlayerPage.setObjectName(u"videoPlayerPage")
+        self.verticalLayout_4 = QVBoxLayout(self.videoPlayerPage)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.videoWidgetFram = QFrame(self.videoPlayerPage)
         self.videoWidgetFram.setObjectName(u"videoWidgetFram")
         self.videoWidgetFram.setFrameShape(QFrame.StyledPanel)
         self.videoWidgetFram.setFrameShadow(QFrame.Raised)
@@ -127,16 +135,16 @@ class Ui_Records(object):
         self.gridLayout.addLayout(self.videoWidgetFramLayout, 0, 0, 1, 1)
 
 
-        self.verticalLayout_2.addWidget(self.videoWidgetFram)
+        self.verticalLayout_4.addWidget(self.videoWidgetFram)
 
         self.horizontalLayout_5 = QHBoxLayout()
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.currentTimeVideo = QLabel(self.frame)
+        self.currentTimeVideo = QLabel(self.videoPlayerPage)
         self.currentTimeVideo.setObjectName(u"currentTimeVideo")
 
         self.horizontalLayout_5.addWidget(self.currentTimeVideo)
 
-        self.slider = QSlider(self.frame)
+        self.slider = QSlider(self.videoPlayerPage)
         self.slider.setObjectName(u"slider")
         self.slider.setSingleStep(1)
         self.slider.setPageStep(20)
@@ -145,18 +153,18 @@ class Ui_Records(object):
 
         self.horizontalLayout_5.addWidget(self.slider)
 
-        self.totalTimeVideo = QLabel(self.frame)
+        self.totalTimeVideo = QLabel(self.videoPlayerPage)
         self.totalTimeVideo.setObjectName(u"totalTimeVideo")
 
         self.horizontalLayout_5.addWidget(self.totalTimeVideo)
 
         self.horizontalLayout_5.setStretch(1, 1)
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout_5)
+        self.verticalLayout_4.addLayout(self.horizontalLayout_5)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.playButton = QPushButton(self.frame)
+        self.playButton = QPushButton(self.videoPlayerPage)
         self.playButton.setObjectName(u"playButton")
         self.playButton.setMinimumSize(QSize(70, 40))
         self.playButton.setMaximumSize(QSize(120, 16777215))
@@ -166,7 +174,7 @@ class Ui_Records(object):
 
         self.horizontalLayout.addWidget(self.playButton)
 
-        self.stopButton = QPushButton(self.frame)
+        self.stopButton = QPushButton(self.videoPlayerPage)
         self.stopButton.setObjectName(u"stopButton")
         self.stopButton.setMinimumSize(QSize(70, 40))
         self.stopButton.setMaximumSize(QSize(120, 16777215))
@@ -177,9 +185,24 @@ class Ui_Records(object):
         self.horizontalLayout.addWidget(self.stopButton)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.verticalLayout_4.addLayout(self.horizontalLayout)
 
-        self.verticalLayout_2.setStretch(0, 1)
+        self.verticalLayout_4.setStretch(0, 1)
+        self.stackedWidget.addWidget(self.videoPlayerPage)
+        self.picutreViewerPage = QWidget()
+        self.picutreViewerPage.setObjectName(u"picutreViewerPage")
+        self.verticalLayout_5 = QVBoxLayout(self.picutreViewerPage)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.pictureViewer = QLabel(self.picutreViewerPage)
+        self.pictureViewer.setObjectName(u"pictureViewer")
+        self.pictureViewer.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_5.addWidget(self.pictureViewer)
+
+        self.stackedWidget.addWidget(self.picutreViewerPage)
+
+        self.verticalLayout_2.addWidget(self.stackedWidget)
+
 
         self.horizontalLayout_2.addWidget(self.frame)
 
@@ -205,13 +228,11 @@ class Ui_Records(object):
         self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.comboBox = QComboBox(self.rightSideBar)
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.setObjectName(u"comboBox")
+        self.title = QLabel(self.rightSideBar)
+        self.title.setObjectName(u"title")
+        self.title.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.comboBox)
+        self.verticalLayout.addWidget(self.title)
 
         self.listWidget = QListWidget(self.rightSideBar)
         self.listWidget.setObjectName(u"listWidget")
@@ -225,20 +246,23 @@ class Ui_Records(object):
 
         self.horizontalLayout_2.setStretch(0, 1)
         self.horizontalLayout_2.setStretch(1, 5)
-        QWidget.setTabOrder(self.radioButton, self.radioButton_2)
-        QWidget.setTabOrder(self.radioButton_2, self.dateTimeEdit)
+        QWidget.setTabOrder(self.videoRadioButton, self.pictureRadioButton)
+        QWidget.setTabOrder(self.pictureRadioButton, self.dateTimeEdit)
         QWidget.setTabOrder(self.dateTimeEdit, self.dateTimeEdit_2)
         QWidget.setTabOrder(self.dateTimeEdit_2, self.searchButton)
 
         self.retranslateUi(Records)
+
+        self.stackedWidget.setCurrentIndex(0)
+
 
         QMetaObject.connectSlotsByName(Records)
     # setupUi
 
     def retranslateUi(self, Records):
         Records.setWindowTitle(QCoreApplication.translate("Records", u"Form", None))
-        self.radioButton.setText(QCoreApplication.translate("Records", u"Videos", None))
-        self.radioButton_2.setText(QCoreApplication.translate("Records", u"Pictures", None))
+        self.videoRadioButton.setText(QCoreApplication.translate("Records", u"Videos", None))
+        self.pictureRadioButton.setText(QCoreApplication.translate("Records", u"Pictures", None))
         self.label.setText(QCoreApplication.translate("Records", u"Start Date", None))
         self.dateTimeEdit.setDisplayFormat(QCoreApplication.translate("Records", u"M/d/yyyy   h:mm AP", None))
         self.label_2.setText(QCoreApplication.translate("Records", u"End Date", None))
@@ -248,10 +272,8 @@ class Ui_Records(object):
         self.totalTimeVideo.setText(QCoreApplication.translate("Records", u"00:00:00", None))
         self.playButton.setText(QCoreApplication.translate("Records", u"Play/Pause", None))
         self.stopButton.setText(QCoreApplication.translate("Records", u"PushButton", None))
+        self.pictureViewer.setText(QCoreApplication.translate("Records", u"TextLabel", None))
         self.toggleSideBarButton.setText("")
-        self.comboBox.setItemText(0, QCoreApplication.translate("Records", u"All Record", None))
-        self.comboBox.setItemText(1, QCoreApplication.translate("Records", u"Videos", None))
-        self.comboBox.setItemText(2, QCoreApplication.translate("Records", u"Pictures", None))
-
+        self.title.setText(QCoreApplication.translate("Records", u"Title", None))
     # retranslateUi
 
