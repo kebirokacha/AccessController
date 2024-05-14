deb7f7d4e5a61ba08b690c30b1584eed4b22257f
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-05-14 22:57:45 +0100

    -Replace the qrc file with new one -Improve main ui (some known bugs)

--- resources/ui/Main_ui.py
+++ resources/ui/Main_ui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'Main.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.6.1
+## Created by: Qt User Interface Compiler version 6.7.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QMainWindow,
     QPushButton, QSizePolicy, QTabWidget, QWidget)
-from . import resources_rc
+from . import main_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -53,7 +53,7 @@ class Ui_MainWindow(object):
 "	background-color: rgb(33, 33, 33);\n"
 "}\n"
 "QTabWidget::pane {\n"
-" border-top:2px solid rgb(0, 142, 246) ;\n"
+" 	border-top:2px solid rgb(0, 142, 246) ;\n"
 "}\n"
 "\n"
 "#mainTab QPushButton{\n"
@@ -68,8 +68,8 @@ class Ui_MainWindow(object):
 "	background-color: rgb(0, 142, 246);\n"
 "	color: rgb(189, 189, 189);\n"
 "    border: 1px solid rgb(189, 189, 189);\n"
-"    border-rad"
-                        "ius: 12px; \n"
+"    border-ra"
+                        "dius: 12px; \n"
 "    padding: 20px; \n"
 "}\n"
 "\n"
@@ -85,12 +85,12 @@ class Ui_MainWindow(object):
         self.tabWidget.setObjectName(u"tabWidget")
         self.tabWidget.setTabletTracking(False)
         self.tabWidget.setStyleSheet(u"")
-        self.tabWidget.setTabShape(QTabWidget.Rounded)
+        self.tabWidget.setTabShape(QTabWidget.TabShape.Rounded)
         self.tabWidget.setIconSize(QSize(30, 30))
-        self.tabWidget.setElideMode(Qt.ElideMiddle)
+        self.tabWidget.setElideMode(Qt.TextElideMode.ElideMiddle)
         self.tabWidget.setDocumentMode(False)
         self.tabWidget.setTabsClosable(True)
-        self.tabWidget.setMovable(True)
+        self.tabWidget.setMovable(False)
         self.tabWidget.setTabBarAutoHide(False)
         self.titleTab = QWidget()
         self.titleTab.setObjectName(u"titleTab")
@@ -105,7 +105,7 @@ class Ui_MainWindow(object):
         self.liveButton = QPushButton(self.mainTab)
         self.liveButton.setObjectName(u"liveButton")
         icon = QIcon()
-        icon.addFile(u":/icons/Icons/circle-dot.png", QSize(), QIcon.Normal, QIcon.Off)
+        icon.addFile(u":/icons/Icons/circle-dot-regular.svg", QSize(), QIcon.Normal, QIcon.Off)
         self.liveButton.setIcon(icon)
         self.liveButton.setIconSize(QSize(30, 30))
 
@@ -114,7 +114,7 @@ class Ui_MainWindow(object):
         self.recordsButton = QPushButton(self.mainTab)
         self.recordsButton.setObjectName(u"recordsButton")
         icon1 = QIcon()
-        icon1.addFile(u":/icons/Icons/Play-back.png", QSize(), QIcon.Normal, QIcon.Off)
+        icon1.addFile(u":/icons/Icons/film-solid.svg", QSize(), QIcon.Normal, QIcon.Off)
         self.recordsButton.setIcon(icon1)
         self.recordsButton.setIconSize(QSize(30, 30))
 
@@ -123,7 +123,7 @@ class Ui_MainWindow(object):
         self.dataBaseButton = QPushButton(self.mainTab)
         self.dataBaseButton.setObjectName(u"dataBaseButton")
         icon2 = QIcon()
-        icon2.addFile(u":/icons/Icons/Data-Base.png", QSize(), QIcon.Normal, QIcon.Off)
+        icon2.addFile(u":/icons/Icons/database-solid.svg", QSize(), QIcon.Normal, QIcon.Off)
         self.dataBaseButton.setIcon(icon2)
         self.dataBaseButton.setIconSize(QSize(30, 30))
 
@@ -132,7 +132,7 @@ class Ui_MainWindow(object):
         self.settingbutton = QPushButton(self.mainTab)
         self.settingbutton.setObjectName(u"settingbutton")
         icon3 = QIcon()
-        icon3.addFile(u":/icons/Icons/gear.png", QSize(), QIcon.Normal, QIcon.Off)
+        icon3.addFile(u":/icons/Icons/gear-solid.svg", QSize(), QIcon.Normal, QIcon.Off)
         self.settingbutton.setIcon(icon3)
         self.settingbutton.setIconSize(QSize(30, 30))
 
@@ -142,7 +142,7 @@ class Ui_MainWindow(object):
         self.gridLayout_2.addLayout(self.horizontalLayout, 0, 0, 1, 1)
 
         icon4 = QIcon()
-        icon4.addFile(u":/icons/Icons/Home.png", QSize(), QIcon.Normal, QIcon.Off)
+        icon4.addFile(u":/icons/Icons/house-solid.svg", QSize(), QIcon.Normal, QIcon.Off)
         self.tabWidget.addTab(self.mainTab, icon4, "")
 
         self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
